d8fbbd8c548824502f6f74d6ce92d7d73ce2555e
Alma Flores <flores.almalee@gmail.com>
2020-08-18 02:11:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown(number)
-  while number > 0  
+  while number > 10  
     puts "#{number} SECOND(S)!"
     number -= 1
-    sleep 1
+    break when 
   end
    sleep 1
    "HAPPY NEW YEAR!"
