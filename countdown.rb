71b5e3bab0cf1211df0627e83f18ccabd8945914
Alma Flores <flores.almalee@gmail.com>
2020-08-18 01:56:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,8 @@
 def countdown(number)
   while number > 0  
     puts "#{number} SECOND(S)!"
-    sleep(0.25)
+    sleep 1
     number -= 1
   end
-  "HAPPY NEW YEAR!"
+  puts "HAPPY NEW YEAR!"
 end
